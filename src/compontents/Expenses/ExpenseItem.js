ce73ea58d3449ff5a5f6d3f80127ab6f67517fb5
managne <agne.manaikaite@gmail.com>
2023-02-15 15:58:33 +0200

    feat: added new Expense item form/ Expense year filter

--- src/compontents/Expenses/ExpenseItem.js
+++ src/compontents/Expenses/ExpenseItem.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    return (
+   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
@@ -12,7 +13,7 @@ const ExpenseItem = (props) => {
         <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
-    );
+  );
 }
 
 export default ExpenseItem;
--- src/compontents/Expenses/Expenses.js
+++ src/compontents/Expenses/Expenses.js
@@ -1,23 +1,33 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import './Expenses.css'
 
 const Expenses = (props) => {
-    
-    return (
-        <Card className="expenses">
-            {
-              props.items.map((item) => (
-                <ExpenseItem
-                  key={item.id}
-                  date={item.date}
-                  amount={item.amount}
-                  title={item.title}
-                />
-              ))
-            }
-        </Card>
-    )
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          date={item.date}
+          amount={item.amount}
+          title={item.title}
+        />
+      ))}
+    </Card>
+  );
 }
 
 export default Expenses;
